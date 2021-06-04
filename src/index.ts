0bf910abf8a0eb4b1738325fab33bdeb410b4c0f
maxmezzomo <maxmezzomo@gmail.com>
2021-06-05 00:29:47 +0100

    export default as named for vec2 and vec3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/math';
-export * from './lib/vec2';
-export * from './lib/vec3';
+export { default as vec2 } from './lib/vec2';
+export { default as vec3 } from './lib/vec3';
 export * from './types/vec2';
 export * from './types/vec3';
